9fa3fec0dd9ec7dbc17c122ad2fffdd33a06d8bd
aislingchan <2467708C@student.gla.ac.uk>
2022-10-20 11:49:21 +0100

    Basic design of comparison page

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,57 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <p>Hello World!</p>
+      <Header title="Sample Course Title"/>
+      <div>
+        <SubmissionContainer side="left"/>
+        <SubmissionContainer side="right"/>
+      </div>
+    </div>
+  )
+}
+
+function Header(props){
+  return (
+    <div>
+      <h1 className="heading">Adaptive Comparative Judgement Tool</h1>
+      <hr/>
+      <h3 className="heading">{props.title}</h3>
+    </div>
+  )
+}
+
+function SubmissionContainer(props){
+  return (
+    <div className="submissionContainer">
+      <SubmissionDisplay/>
+      <button>{props.side} is better</button>
+      <CommentsContainer/>
+    </div>
+  )
+}
+
+function SubmissionDisplay(){
+  return (
+    <div>
+      <p style={{borderStyle: "solid", borderWidth: "1px"}}>Display PDF of script here</p>
+    </div>
+  )
+}
+
+function CommentsContainer(){
+  return (
+    <div>
+      <CommentsBox audience="students"/>
+      <CommentsBox audience="admin staff"/>
+    </div>
+  )
+}
+
+function CommentsBox(props){
+  return (
+    <div>
+      <p>Comments for {props.audience}</p>
+      <textarea></textarea>
     </div>
   )
 }
